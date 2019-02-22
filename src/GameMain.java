d06e7f634430c2918361ed9bd91de9832321ed13
Marek.Czernohous <Marek.Czernohous@DESKTOP-JUVVB7M.2631.skool.net>
2019-02-22 09:07:13 +0100

    Commit 1. Stunde

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,4 +1,15 @@
-
 public class GameMain {
-
+   
+	public static void main(String args[]){
+		
+	Position position1 = new Position(5,6);
+	Position position2 = new Position(5,6);
+	WallTile wand = new WallTile(position1);
+	
+	System.out.println(wand);	
+	System.out.println(position1.equals(position2));
+	position1.setX(4);
+	System.out.println(position1.equals(position2));
+	}
+	
 }

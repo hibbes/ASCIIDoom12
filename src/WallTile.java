db0f6694001cdc28b4c5da9a1cf525beaacc5e2f
Marek.Czernohous <Marek.Czernohous@DESKTOP-JUVVB7M.2631.skool.net>
2019-03-15 08:12:06 +0100

    Player-Funktionalitäten

--- src/WallTile.java
+++ src/WallTile.java
@@ -1,15 +1,6 @@
 public class WallTile extends GameTile{
 
-	public WallTile(Position position) {
-		super(position);
-		
-	}
 	
-	public WallTile() {
-		super();
-		
-	}
-
 	public String toString(){
 		return "#";
 	}

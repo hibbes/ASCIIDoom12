fa7c113b638ef51bf12f415182e32468502b299d
hibbes <marek@czernohous.de>
2019-03-15 07:20:28 +0100

    Div. Updates

--- src/GameMain.java
+++ src/GameMain.java
@@ -2,14 +2,9 @@ public class GameMain {
    
 	public static void main(String args[]){
 		
-	Position position1 = new Position(5,6);
-	Position position2 = new Position(5,6);
-	WallTile wand = new WallTile(position1);
-	
-	System.out.println(wand);	
-	System.out.println(position1.equals(position2));
-	position1.setX(4);
-	System.out.println(position1.equals(position2));
+	 GameField gamefield= new GameField(7,8,"###   ### # ####### ### ### ####  #### ####### #### ####");
+	 
+	 System.out.println(gamefield);
 	}
 	
 }
